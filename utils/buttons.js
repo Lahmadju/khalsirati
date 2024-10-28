4107f8cafa26ebf40d84e3569d851ff81e533c53
Muslim <musievmuslim0@gmail.com>
2024-10-28 17:58:20 +0300

    khalsiratii

---
+++ utils/buttons.js
@@ -0,0 +1,16 @@
+// Массив кнопок для каждой социальной сети
+const socialNetworks = [
+  { name: 'Telegram', url: 'https://t.me/khalsiratii', type: 'social' },
+];
+
+// Массив кнопок для каждой категории промокодов и скидок
+const promoCodes = [
+  { 
+    name: 'Хиджаб: Основные требования', 
+    url: 'https://telegra.ph/Hidzhab-osnovnye-trebovaniya-10-06', 
+    description: 'Все о хиджабе, как правильное его носить и т.д', 
+    type: 'promo' 
+  },
+];
+
+module.exports = { socialNetworks, promoCodes };
