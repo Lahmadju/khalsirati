f8e724e019e365595d3347d6f3774ab96f3f3c96
Muslim <musievmuslim0@gmail.com>
2024-11-02 16:18:05 +0300

    start

--- utils/buttons.js
+++ utils/buttons.js
@@ -1,6 +1,7 @@
 // Массив кнопок для каждой социальной сети
 const socialNetworks = [
   { name: 'Telegram', url: 'https://t.me/khalsiratii', type: 'social' },
+  { name: 'Instagram', url: 'https://instagram.com/khalsirati?igsh=MzRlODBiNWFlZA==', type: 'social' },
 ];
 
 // Массив кнопок для каждой категории промокодов и скидок
